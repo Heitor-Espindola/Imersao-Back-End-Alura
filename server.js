58dce646a370c05121331f1daba7f34742a2e7ba
Heitor Espíndola Testi <heitoresp.dev@gmail.com>
2025-02-06 22:15:47 -0300

    Aula 2

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descrição: "Uma foto teste",
+        imagem: "https://placecats.com/millie/200/300",
+    },
+    {
+        id: 2,
+        descrição: "Gatinho brincando",
+        imagem: "https://placecats.com/400/500",
+    },
+    {
+        id: 3,
+        descrição: "Gatinho deitado",
+        imagem: "https://placecats.com/600/700",
+    },
+    {
+        id: 4,
+        descrição: "Gatinho Curioso",
+        imagem: "https://placecats.com/700/499",
+    },
+]
+
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
     console.log("Servidor escutando...");
     })
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas Vindas à imersão!");
-})
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+// acesse HTTP.cat para encontrar todos os status
+
+function buscarPostPorID(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id);
+    res.status(200).json(posts[index]);
+});
